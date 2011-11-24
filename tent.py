bef079614270b6e81260ca55d0308273cbf89ff9
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2011-11-25 00:36:46 +0100

    Update CLI to use suite files.

--- tent.py
+++ tent.py
@@ -8,31 +8,43 @@ import argparse, sys
 
 parser = argparse.ArgumentParser( description='OCCI tent command line interface', epilog=None )
 parser.add_argument( '--version', action='version', version='OCCI tent v1.0' )
-parser.add_argument( '--config', default='config.yaml', type=open, help='configuration file (default: %(default)s)', metavar='FILE' )
+parser.add_argument( '--config', '-c', default='config.yaml', type=open, help='configuration file (default: %(default)s)', metavar='FILE' )
 parser.add_argument( '--modules', action='store_true', help='list all available test modules' )
-parser.add_argument( '-l', '--list', action='store_true', help='list all available tests' )
-parser.add_argument( '--show', help='show test or test module information', metavar='TEST' )
-parser.add_argument( '--file', '-f', default='tests.yaml', help='run tests from test file' )
-parser.add_argument( 'tests', nargs='*', help='tests or test suites to run' )
+
+parser.add_argument( '--list', '-l', action='store_true', help='list available test cases from test suite' )
+parser.add_argument( '--run', '-r', nargs='?', const=-1, type=int, help='run single test case from test suite', metavar='ID' )
+parser.add_argument( '--runmod', help='run single, parameterless test module', metavar='MODULE' )
+parser.add_argument( 'suite', nargs='?', type=open, help='test suite file to use' )
 
 def main ():
-	args = parser.parse_args()
+	try:
+		args = parser.parse_args()
+	except IOError as e:
+		parser.error( str( e ) )
 	tent = Tent( args.config )
-
+	
 	if args.modules:
 		printModuleList( tent )
 		parser.exit()
 	
-	if len( args.tests ) > 0:
-		for test in args.tests:
-			try:
-				tent.runTest( test, None )
-			except ValueError:
-				print( 'Test not found: ' + test )
-	else:
-		print( 'Running tests from `{0}` file'.format( args.file ) )
-		tent.runTestsFromFile( args.file )
+	if args.runmod:
+		try:
+			tent.runTest( args.runmod, None )
+		except ValueError:
+			parser.error( 'test module not found: ' + test )
+		parser.exit()
+	
+	if not args.suite:
+		parser.error( 'no test suite file given' )
+	
+	if args.list:
+		parser.error( 'Sorry, not yet implemeted.' )
+	
+	if args.run:
+		parser.error( 'Sorry, not yet implemeted.' )
 
+	print( 'Running tests from `{0}`'.format( args.suite.name ) )
+	tent.runTestsFromFile( args.suite )
 
 def printModuleList ( tent ):
 	for module in tent.modules:
